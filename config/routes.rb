1bddb5d42d91791d759f1f6d8ef5d68230929ba1
Noor Alam <nooralamjaturi@gmail.com>
2023-06-24 17:35:03 -0500

    Hunzai's todolist

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'homepage#index'
 
   # Add routes below this line
+  get 'tasks' => 'tasks#index'
 
   # Add routes below above line
+  post 'tasks' => 'tasks#create'
+
+  delete 'tasks/:id' => 'tasks#destroy'
+
+  put '/tasks/:id/mark_complete' => 'tasks#mark_complete'
+
+  put '/tasks/:id/mark_active'   => 'tasks#mark_active'
 
   # Redirect all other paths to index page, which will be taken over by AngularJS
   get '*path' => 'homepage#index'
